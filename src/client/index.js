7f0e53fc405aa33fdcf4b2b50a994e4cd27d2d21
ServinCharles <servin.suriya@gmail.com>
2020-02-14 21:02:28 +0000

    test

---
+++ src/client/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+state = {
+  count: 2,
+};
+
+Multiply = () => {
+
+  const currentnumber = this.state.count;
+
+  this.setState({
+  // eslint-disable-next-line no-magic-numbers
+    count: currentnumber * 2,
+  });
+
+}
+divide = () => {
+
+  const currentnumber = this.state.count;
+
+  this.setState({
+  // eslint-disable-next-line no-magic-numbers
+    count: currentnumber / 2,
+  });
+
+}
+
+render() {
+  return <div>
+    <h1>{this.state.count}</h1>
+    <button onClick={this.Multiply}>+</button>
+    <button onClick={this.divide}>-</button>
+  </div>
+}
+}
+
+render(<App />, document.getElementById('root'));
